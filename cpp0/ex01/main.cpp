e73154293c1a6b5654adead77c3d86952d572502
Asier Arranz Lopez <asierarranz99@gmail.com>
2025-02-18 14:33:05 +0100

    empezando a trabalhar

--- cpp0/ex01/main.cpp
+++ cpp0/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aarranz- <aarranz-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 11:38:21 by aarranz-          #+#    #+#             */
-/*   Updated: 2025/02/11 12:53:10 by aarranz-         ###   ########.fr       */
+/*   Updated: 2025/02/18 11:55:25 by aarranz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 int main(void)
 {
+	PhoneBook	phonebook;
 	std::string	line;
 
 	while(1)
 	{
 		std::cout << "▶️ ";
 		std::getline(std::cin, line);
-//		if (line.compare("ADD") == 0)
+		if (line.compare("ADD") == 0)
+			phonebook.add_contact();
 //		if (line.compare("SEARCH") == 0)
 		if (line.compare("EXIT") == 0)
 			break;
--- cpp0/ex01/phonebook.hpp
+++ cpp0/ex01/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: aarranz- <aarranz-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 11:39:21 by aarranz-          #+#    #+#             */
-/*   Updated: 2025/02/11 12:07:09 by aarranz-         ###   ########.fr       */
+/*   Updated: 2025/02/18 12:01:03 by aarranz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
-//#include "Contact.hpp"
 
 class PhoneBook
 {
@@ -25,15 +24,7 @@ public:
 	~PhoneBook();
 
 	void	add_contact();
-//	void	search_contact();
 
-private:
-//	Contact contacts[8];
-//	int		stored_contacts;
-//	int		last_contact;
 };
-
-
-//bool	is_number(std::string input);
 
 #endif
